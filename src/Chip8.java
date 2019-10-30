887319a64b6787cf3d8a4288b3c68c349dd6893d
Saad Bhatti <saadbhatti111@gmail.com>
2019-10-29 20:06:17 -0500

    finished loadROM

--- src/Chip8.java
+++ src/Chip8.java
@@ -9,15 +9,15 @@ public class Chip8 {
 
 
     //FIXME: The stack should probably be like an array or something
-    private Stack<Integer> stack;
+    private Stack<Short> stack;
     private short pc;
     private short I;
     private byte sp;
     private byte memory[];
     private byte registers[];
     private byte framebuffer[];
-    private int soundTimer;
-    private int delayTimer;
+    private byte soundTimer;
+    private byte delayTimer;
 
 
 
@@ -34,19 +34,43 @@ public class Chip8 {
         delayTimer = 0;
 
         //load fontset from 0x000 to 0x080 in memory
+        int fontSet[] = {
+                0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+                0x20, 0x60, 0x20, 0x20, 0x70, // 1
+                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+                0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+        int i = 0;
+        for(int f : fontSet){
+            memory[i++] = (byte)f;
+        }
+
 
     }
 
     public void loadROM(String filename) throws IOException {
         //load ROM into memory starting from 0x200 to 0xfff
 
+        //read the shorts from the chip8 file
         FileInputStream fin = new FileInputStream(filename);
         DataInputStream din = new DataInputStream(fin);
         int i = 0;
         while(din.available() > 0){
             short s = din.readShort();
 
-
+            //break it up into bytes and store in memory
             byte firstByte = (byte)(((s & 0xff00) >> 8));
             byte secondByte = (byte)((s & 0x00ff));
 
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        System.out.println("|====== Chip-8 Emulator ======|\n");
+        System.out.println("\n|====== Chip-8 Emulator ======|\n");
         Chip8 myChip8 = new Chip8();
-        String FILE_PATH = "res/maze.ch8";
+        String FILE_PATH = "res/stars.ch8";
         myChip8.loadROM(FILE_PATH);
     }
 }
