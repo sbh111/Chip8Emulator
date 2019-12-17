a28c1b313ddeae38bc87794beb70527f0c09bba6
Saad Bhatti <saadbhatti111@gmail.com>
2019-12-16 22:28:56 -0600

    got graphics to work

--- src/Main.java
+++ src/Main.java
@@ -19,15 +19,18 @@ public class Main extends Application{
 
     int WINDOW_W = 800;
     int WINDOW_H = 400;
+    float offset = WINDOW_W/30; //offset from top and side edges
 
-    float pixelW = 64 * WINDOW_W/100;
-    float pixelH = 32 * WINDOW_H/40;
-    float pixelX = WINDOW_W/20;
-    float pixelY = WINDOW_H/20;
+    float blockW = WINDOW_W/100;
+    float blockH = WINDOW_H/40;
+    float pixelW = 64 * blockW;
+    float pixelH = 32 * blockH;
+    float pixelX = offset;
+    float pixelY = offset;
 
     float valuesW = 20 * WINDOW_W/100;
     float valuesH = pixelH;
-    float valuesX = WINDOW_W - (valuesW + WINDOW_W/20);
+    float valuesX = WINDOW_W - (valuesW + offset);
     float valuesY = pixelY;
 
 
@@ -36,10 +39,28 @@ public class Main extends Application{
         launch(args);
     }
 
-    public void start(Stage primaryStage) throws IOException{
+    public void drawGfx(char[] frameBuffer, GraphicsContext gc){
+        for(int i = 0; i < 64; i++){
+            for(int j = 0; j < 32; j++){
+                float x = (i * blockW) + offset;
+                float y = (j * blockH) + offset;
+
+                int loc = i*32 + j;
+                if((int)frameBuffer[loc] == 1){
+                    gc.setFill(Color.WHITE);
+                }
+                else{
+                    gc.setFill(Color.MIDNIGHTBLUE);
+                }
+                gc.fillRect(x, y, blockW, blockH);
+            }
+        }
+    }
+
+    public void start(Stage primaryStage) throws IOException {
         //main code here
         Chip8 myChip8 = new Chip8();
-        myChip8.loadROM("res/stars.ch8", true);
+        myChip8.loadROM("res/maze.ch8", true);
 
         primaryStage.setTitle("Chip-8 Emulator");
 
@@ -62,23 +83,25 @@ public class Main extends Application{
         GraphicsContext valuesGC = values.getGraphicsContext2D();
 
         final long startNanoTime = System.nanoTime();
-        new AnimationTimer(){
-            public void handle(long currentNanoTime){
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
                 double elsapsedTime = (currentNanoTime - startNanoTime) / 1000000000.0;
 
+                //emulate 1 cycle
                 myChip8.emulateCycle(keys);
-                StringBuilder bldr = new StringBuilder();
 
+                //build the emulator values string
+                StringBuilder bldr = new StringBuilder();
                 bldr.append(String.format("OpCode:\t0x%s\n", Integer.toHexString(0xffff & myChip8.getOpCode())));
                 bldr.append(String.format("PC:\t0x%s\n", Integer.toHexString(myChip8.getPc())));
                 bldr.append(String.format("I:\t0x%s\n", Integer.toHexString(myChip8.getI())));
 
-                for(int i = 0; i < 16; i++){
+                for (int i = 0; i < 16; i++) {
                     bldr.append(String.format("V%d:\t0x%s\n", i, Integer.toHexString(myChip8.getRegister(i))));
                 }
 
-                bldr.append(String.format("DelayTimer:\t%d\n", (int)myChip8.getDelayTimer()));
-                bldr.append(String.format("SoundTimer:\t%d\n", (int)myChip8.getSoundTimer()));
+                bldr.append(String.format("DelayTimer:\t%d\n", (int) myChip8.getDelayTimer()));
+                bldr.append(String.format("SoundTimer:\t%d\n", (int) myChip8.getSoundTimer()));
                 String valuesTxt = bldr.toString();
 
 
@@ -86,18 +109,22 @@ public class Main extends Application{
                 valuesGC.setFill(Color.MIDNIGHTBLUE);
                 valuesGC.fillRect(valuesX, valuesY, valuesW, valuesH);
                 valuesGC.setFill(Color.BLACK);
+                valuesGC.setLineWidth(5);
                 valuesGC.strokeRect(valuesX, valuesY, valuesW, valuesH);
 
-
-
-                pixelsGC.setFill(Color.MIDNIGHTBLUE);
-                pixelsGC.fillRect(pixelX, pixelY, pixelW, pixelH);
-
+                //Draw the emulator values
                 valuesGC.setFill(Color.WHITE);
                 valuesGC.setFont(new Font("Arial", 13));
-                valuesGC.fillText(valuesTxt, valuesX + 1, valuesY + WINDOW_H/40 + 1, valuesW);
+                valuesGC.fillText(valuesTxt, valuesX + 1, valuesY + WINDOW_H / 40 + 1, valuesW);
+
+                //Draw the emulator graphics
+                drawGfx(myChip8.getFramebuffer(), pixelsGC);
+
+                pixelsGC.setFill(Color.BLACK);
+                pixelsGC.setLineWidth(5);
+                pixelsGC.strokeRect(pixelX, pixelY, pixelW, pixelH);
             }
         }.start();
         primaryStage.show();
-    }
-}
+    }//end start class
+}//end Main class
